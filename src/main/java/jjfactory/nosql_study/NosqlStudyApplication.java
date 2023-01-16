d7dfb987a79ee1c7e7d2ebe60d8beca7f8ac4823
jaehyeong2 <wogud1514@naver.com>
2023-01-16 17:29:43 +0900

    스타트

---
+++ src/main/java/jjfactory/nosql_study/NosqlStudyApplication.java
@@ -0,0 +1,13 @@
+package jjfactory.nosql_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosqlStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NosqlStudyApplication.class, args);
+    }
+
+}
